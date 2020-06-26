fd81b7d90d7ddc4dc59d15d3af6e375b34bbfaa6
BrownKarateMaster <muqrizx@gmail.com>
2020-06-26 16:14:08 +0800

    Code update
    minor edit to the codes arrangement

--- twitter_analyzer_main.py
+++ twitter_analyzer_main.py
@@ -9,89 +9,73 @@ import re
 import tweepy
 import twitter_analyzer_auth as twa
 import pandas as pd
-import numpy as np
 import nltk
 from nltk.corpus import stopwords
 from collections import Counter
 import matplotlib.pyplot as plt
 
-keyword = "umno"
-
-api = tweepy.API(twa.auth,wait_on_rate_limit=True)
-
-public_tweets = api.home_timeline()
-#for tweet in public_tweets:
-    #print(tweet.text)
-
-search_tweet = api.search(q=keyword)
+keyword = "covid"
+num_tweets = 100
 
 stop_words = stopwords.words('english')
 added_sw = ['dont','get','youre','aint','thats','would','know','cant'
-            ,'shes','hes','im','like','rt']
+           ,'shes','hes','im','like','rt']
 
+def get_tweets(query, count):
+    #get tweets from twitter
+   api = tweepy.API(twa.auth,wait_on_rate_limit=True)
+   search_tweet = tweepy.Cursor(api.search, query, lang="en").items(count)
+   
+   text_arr = []
+   for tweet in search_tweet:
+       text_arr.append(tweet.text)
+   
+  
+   return text_arr
+    
 def add_stop_words(arr):
+    #add stop words that was initialized above
 	for e in arr:
 		stop_words.append(e)
 
-add_stop_words(added_sw)
-
-
-def filter_tweet(keyword):
-    search_tweet = tweepy.Cursor(api.search, q=keyword, lang="en").items(100)
-    #df = pd.DataFrame(t.__dict__ for t in search_tweet)
-    #arr = df.to_numpy()
-    #tweet_column = arr[:, [30]]
-    text_arr = []
-    for tweet in search_tweet:
-        text_arr.append(tweet.text)
-    #text_arr = df.to_numpy()
-    
-    return text_arr
-    
-    #return tweet_column
-    #print(tweet_column)
-
-
-def search_tweet(keyword):
-    tweet_list = []
-    for tweet in tweepy.Cursor(api.search, q=keyword).items(50):
-        tweet_list.append(tweet.text)
-    
-    return tweet_list
-
-
-def clean_tweets(arr):
-    remove_caps = [word.lower() for word in arr] 
-    separator = ', '
-    join_test = separator.join(remove_caps)
-    removeurl = " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", join_test).split())
-    text = nltk.word_tokenize(removeurl)
-    tweet_nsw = [word for word in text if not word in stop_words]
-    tweet_nsw_nc = [word for word in tweet_nsw if not word in keyword]
-    
-    
-    return tweet_nsw_nc
-
+def clean_and_tokenize(arr):
+    #clean up tweets by lowering caps, tokenize sentences into words, and remove stop words
+   remove_caps = [word.lower() for word in arr]
+   separator = ', '
+   join_tweets = separator.join(remove_caps)
+   removeurl = " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", join_tweets).split())
+   text = nltk.word_tokenize(removeurl)
+   #removing stop words
+   tweet_nsw = [word for word in text if not word in stop_words]
+   tweet_nsw_nc = [word for word in tweet_nsw if not word in keyword]
+  
+   return tweet_nsw_nc
 
 def count_and_plot(arr):
-    nsw_counts = Counter(arr).most_common(20)
-    print(nsw_counts)
-    tweet_nsw_nc = pd.DataFrame(nsw_counts, columns=['words', 'count'])
-    tweet_nsw_nc.head()
-    fig, ax = plt.subplots(figsize=(8, 8))
+    #count most common words mentioned in tweets
+   word_count = Counter(arr).most_common(20)
+   print(word_count)
+   word_count = pd.DataFrame(word_count, columns=['words', 'count'])
+   word_count.head()
+   fig, ax = plt.subplots(figsize=(8, 8))
 
-    # Plot horizontal bar graph
-    tweet_nsw_nc.sort_values(by='count').plot.barh(x='words',
-                            y='count', ax=ax, color="purple")
-    ax.set_title("Common Words Found in tweet search :" + keyword)
-    plt.show()
-    
-    print(tweet_nsw_nc)
+   # Plot horizontal bar graph
+   word_count.sort_values(by='count').plot.barh(x='words',
+                           y='count', ax=ax, color="purple")
+   ax.set_title("Common Words Found in tweet search :" + keyword)
+   plt.show()
+  
+   print(word_count)
 
 
-tweet_arr = filter_tweet(keyword)
-#print(tweet_arr)
-dem_tweets = clean_tweets(tweet_arr)
-count_and_plot(dem_tweets)
+if __name__ == "__main__":
+    add_stop_words(added_sw)
+    tweets = get_tweets(keyword, num_tweets)
+    processed_tweets = clean_and_tokenize(tweets)
+    count_and_plot(processed_tweets)
+
+
+
+
 
 
